4f11f469408258a3ed0465739a2160e82691b9b7
DIVI MANI SANKAR <manisankar.divi@gmail.com>
2026-04-23 23:58:52 +0530

    variables

---
+++ 2.variables.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+#variables can be used to store strings, numbers, and other data types
+
+PERSON1="Mani"
+PERSON2="pooja"
+
+echo "hello $PERSON1 this is $PERSON2"
+echo "$PERSON1 i love you"
+echo "will you marry me $PERSON1"
+echo "$PERSON2 i love you too, i want to marry you"
