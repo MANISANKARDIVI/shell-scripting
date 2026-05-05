1bd44993719cd4ca1db3f510bb69abc3a9530dca
DIVI MANI SANKAR <manisankar.divi@gmail.com>
2026-05-05 08:20:32 +0530

    shell scripting

---
+++ 10.install.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+USERID=$(id -u)
+
+if [ ($USERID) -ne 0 ]
+then
+    echo "You don't have Root access permissions"
+    exit 1
+else
+    echo "you are running with Root Access"
+fi
+
+dnf install mysql -y
