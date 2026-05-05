a105c543e09738cb63f5ef0d63369a757b86f7dd
DIVI MANI SANKAR <manisankar.divi@gmail.com>
2026-05-05 09:07:13 +0530

    shell script

--- 10.install.sh
+++ 10.install.sh
@@ -4,10 +4,24 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "You don't have Root access permissions"
-    exit 1
+	echo "Error: You dont have Root permissons..."
+	exit 1
 else
-    echo "you are running with Root Access"
+	echo "You have Root Access..."
 fi
 
-dnf install mysql -y
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+	echo "Mysql is not installed... Now its installing"
+	dnf install mysql -d
+	if [ $? -ne 0 ]
+	then
+		echo "Error: Mysql is not installed.."
+		exit 1
+	else
+		echo "Mysql is successfully installed..."
+		
+else 
+	echo "Mysql is already installed..."
