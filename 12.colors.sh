1ae447b4ab00177aabd065f69bb668c4ae7d16e2
DIVI MANI SANKAR <manisankar.divi@gmail.com>
2026-05-14 10:51:04 +0530

    colors sh

--- 12.colors.sh
+++ 12.colors.sh
@@ -2,7 +2,7 @@
 
 # Colors 
 
-echo -e "\e[31m this is red color\e[0m]"
+echo -e "\e[31m this is red color\e[0m"
 
 echo "this is color"
 
@@ -10,8 +10,8 @@ echo "this is color"
 
 # we neeed to close the color after printing red color
 
-echo -e "\e[32m this is green color\e[0m]"
+echo -e "\e[32m this is green color\e[0m"
 
-echo -e "\e[33m this is yellow color\e[0m]"
+echo -e "\e[33m this is yellow color\e[0m"
 
-echo -e "\e[34m this is blue color\e[0m]"
+echo -e "\e[34m this is blue color\e[0m"
