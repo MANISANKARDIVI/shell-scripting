e631f45888116f6aee424b8ea9022fa2ff2b41f6
DIVI MANI SANKAR <manisankar.divi@gmail.com>
2026-05-14 10:42:07 +0530

    colors in shell

---
+++ 12.colors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Colors 
+
+echo -e "\e[31m this is red color"
+
+echo -e "\e[32m this is green color]"
+
+echo -e "\e[33m this is yellow color"
+
+echo -e "\e[34m this is blue color"
+
+echo -e "\e[37m this is white color"
