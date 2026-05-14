beed66b142f7ee0526fb7d88f02299ba54f6f97c
DIVI MANI SANKAR <manisankar.divi@gmail.com>
2026-05-14 11:00:11 +0530

    colors shell

---
+++ 13.colors.sh
@@ -0,0 +1,51 @@
+#!/bin/env bash
+
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+B=\e[34m
+W=\e[37m
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "$R ERROR:: You must be root to run this script"
+    exit 1
+else 
+    echo "$Y INFO:: You are root, you can run this script"
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo "$R ERROR:: $2 failed to install"
+        exit 1
+    else 
+        echo "$G INFO:: $2 installed successfully"
+    fi
+}
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then
+    echo "$Y INFO:: Nginx is not installed, installing now"
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo "$G INFO:: Nginx is already installed"
+fi
+
+
+dnf list installed java 
+
+if [ $? -ne 0 ]
+then 
+    echo "$Y INFO:: Java is not installed, installing now"
+    dnf install java -y
+    VALIDATE $? "Java"
+else
+    echo "$G INFO:: Java is already installed"
+fi
+
