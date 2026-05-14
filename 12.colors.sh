128463b36ba16770d9d3e9e2384b41e211b53cc7
DIVI MANI SANKAR <manisankar.divi@gmail.com>
2026-05-14 10:49:40 +0530

    colors sh

--- 12.colors.sh
+++ 12.colors.sh
@@ -4,8 +4,14 @@
 
 echo -e "\e[31m this is red color\e[0m]"
 
-echo  "32m this is green color"
+echo "this is color"
 
 # above 2 lines are having same red color because we have not reset the color after printing red color
 
 # we neeed to close the color after printing red color
+
+echo -e "\e[32m this is green color\e[0m]"
+
+echo -e "\e[33m this is yellow color\e[0m]"
+
+echo -e "\e[34m this is blue color\e[0m]"
